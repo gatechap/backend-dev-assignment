d2ef7a141130028e0b986767776be1135b1707c7
nitacamel <nichanita24@gmail.com>
2024-11-20 22:30:00 +0700

    Assignment 01

---
+++ src/main/java/com/scb/backend_dev_assignment/BackendDevAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.scb.backend_dev_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scb/backend_dev_assignment/BackendDevAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.backend_dev_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
