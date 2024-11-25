0d868ae87c50197cfb6718c04043e2aa49ada94f
nitacamel <nichanita24@gmail.com>
2024-11-26 00:12:58 +0700

    Assignment 14

--- src/main/java/com/scb/backend_dev_assignment/dto/UserDto.java
+++ src/main/java/com/scb/backend_dev_assignment/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.scb.backend_dev_assignment.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserDto {
     private Long id;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
     private String email;
 }
